7f8f826482684d634e849c6f04f3268fbd51f58a
valerielinc-ops <valerielinc-ops@users.noreply.github.com>
2026-02-15 13:14:41 +0000

    deploy: f03335dd6f8c3dc731025009defa07a622514152

---
+++ assets/chevron-up-BE8kCsE0.js
@@ -0,0 +1,7 @@
+import{c as o}from"./index-DjwclMrQ.js";/**
+ * @license lucide-react v0.563.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const c=[["path",{d:"m18 15-6-6-6 6",key:"153udz"}]],n=o("chevron-up",c);export{n as C};
+//# sourceMappingURL=chevron-up-BE8kCsE0.js.map
